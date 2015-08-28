f3d2be4711865681890268978b3c866de9754557
Annie <anniehoogendoorn@gmail.com>
2015-08-28 14:08:27 -0700

    Add Brand class methods setName, getName and  getId. Add BrandTest with testGetName and testGetId.

---
+++ tests/BrandTest.php
@@ -0,0 +1,56 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once 'src/Brand.php';
+    require_once 'src/Brand.php';
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BrandTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            // Brand::deleteAll();
+            // Store::deleteAll();
+        }
+
+        function testGetName()
+        {
+            //Arrange
+            $name = "Brand 1";
+            $test_brand = new Brand($name);
+
+            //Act
+            $result = $test_brand->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function testGetId()
+        {
+            //Arrange
+            $name = "Brand 1";
+            $id = 1;
+            $test_brand = new Brand($name, $id);
+
+            //Act
+            $result = $test_brand->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+        }
+
+        
+
+
+    }
+
+ ?>
